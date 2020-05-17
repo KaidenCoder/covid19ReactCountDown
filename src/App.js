97d81489c7fbc760a332e0b3aaee7050aecbf983
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-05-17 20:10:59 +0530

    lockdown 4.0

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Countdown from './Countdown';
 function App() {
   const currentDate = new Date();
   console.log(currentDate.getMonth())
-  const year = (currentDate.getMonth() === 4 && currentDate.getDate() === 17) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+  const year = (currentDate.getMonth() === 5 && currentDate.getDate() === 31) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
 
 
   return (
     <>
       <div className="year">
-        17 May 2020
+        31 May 2020
       </div>
       <h3>COVID-19 INDIA</h3>
       <br></br>
-      <h1>Lockdown Ends in</h1>
-      {<Countdown date={`${year}-05-17T23:59:59`} /> || <img src={logo} alt="Loading..." id="loading" className="loading" />}
+      <h1>Lockdown 4.0 Ends in</h1>
+      {<Countdown date={`${year}-05-31T23:59:59`} /> || <img src={logo} alt="Loading..." id="loading" className="loading" />}
       {/* <img src={logo} alt="Loading..." id="loading" className="loading" /> */}
     </>
   );
